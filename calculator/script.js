abcfbc8c28a33957b70f47266a2b87e27440674f
Allan <martinezallan123@gmail.com>
2022-07-06 15:10:12 -0700

    modified JS, -redundancy, -unecessary classes

--- calculator/script.js
+++ calculator/script.js
@@ -4,67 +4,48 @@
 const allNums = document.querySelectorAll('.num-btn');
 const display = document.querySelector('h2');
 const clearBtn = document.querySelector('.clear');
-const minusBtn = document.querySelector('.minus');
-const addBtn = document.querySelector('.add');
-const divideBtn = document.querySelector('.divide');
-const multiplyBtn = document.querySelector('.multiply');
 const equalBtn = document.querySelector('.equal-btn');
+const opBtns = document.querySelectorAll('.op-btn')
 
 let previous = null
 let current = null
 let op = null
 
+opBtns.forEach(btn => {
+    btn.addEventListener('click',setOp)
+
+})
+function setOp(){
+    switch(display.innerText){
+
+        case '': display.innerText = '-'         
+                 break;
+
+        default: previous = Number(display.innerText)
+                 op = this.innerText
+                 clearDisplay()
+                 break;
+    }
+}
 
 allNums.forEach(element => {
-    element.addEventListener('click',log);
+    element.addEventListener('click',logNum);
     
 });
 
-function log(){
+function logNum(){
     display.innerText += this.innerText;
 
 }
 
-clearBtn.addEventListener('click',clear);
-function clear(){
+clearBtn.addEventListener('click',clearDisplay);
+function clearDisplay(){
     display.innerText = ''
 }
 
-minusBtn.addEventListener('click',subtract)
-function subtract(){
-    if(display.innerText == ''){
-        console.log('ok')
-        display.innerText = '-'
-    }
+equalBtn.addEventListener('click',calculate)
+function calculate(){
 
-    else {previous = Number(display.innerText)
-    op = '-'
-    clear()}
-}
-
-addBtn.addEventListener('click',add)
-function add(){
-    previous = Number(display.innerText)
-    op = '+'
-    clear()
-}
-
-divideBtn.addEventListener('click',divide)
-function divide(){
-    previous = Number(display.innerText)
-    op = '/'
-    clear()
-}
-
-multiplyBtn.addEventListener('click',multiply)
-function multiply(){
-    previous = Number(display.innerText)
-    op = '*'
-    clear()
-}
-
-equalBtn.addEventListener('click',equal)
-function equal(){
     current = Number(display.innerText)
 
     switch(op){
